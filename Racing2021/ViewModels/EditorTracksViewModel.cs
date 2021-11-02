d99a06c78a160a0fee967db687bc0879bda1525a
Olav Hendrickx <olav.hendrickx@cegeka.com>
2021-11-02 12:16:55 +0100

    Add combobox with tracktiles

--- Racing2021/ViewModels/EditorTracksViewModel.cs
+++ Racing2021/ViewModels/EditorTracksViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Racing2021.Models;
+using Racing2021.Models.Enums;
 using Racing2021.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,10 +16,12 @@ namespace Racing2021.ViewModels
     {
         private ITrackService _trackService;
         private ObservableCollection<Track> _tracks;
+        private ObservableCollection<string> _trackTiles;
         private Track _selectedTrack;
         private RelayCommand _saveChangesCommand;
 
         public ObservableCollection<Track> Tracks { get => _tracks; set { _tracks = value; RaisePropertyChanged(); } }
+        public ObservableCollection<string> TrackTiles { get => _trackTiles; set { _trackTiles = value; RaisePropertyChanged(); } }
 
         public RelayCommand SaveChangesCommand => _saveChangesCommand ??= new RelayCommand(SaveChanges);
 
@@ -37,11 +40,23 @@ namespace Racing2021.ViewModels
             _trackService = trackService;
 
             _tracks = new ObservableCollection<Track>(trackService.GetTracks());
+
+            CreateListOfTrackTiles();
         }
 
         private void SaveChanges()
         {
             _trackService.CreateTracks(Tracks);
+        }
+
+        private void CreateListOfTrackTiles()
+        {
+            TrackTiles = new ObservableCollection<string>();
+
+            foreach (var trackTile in Enum.GetNames(typeof(TrackTile)))
+            {
+                TrackTiles.Add(trackTile);
+            }
         }
     }
 }
